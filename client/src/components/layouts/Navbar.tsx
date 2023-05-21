979a41b7d4cb3001d0634f21e20fe9024f47164d
hokageCV <chaitanyavaru@gmail.com>
2023-05-21 15:05:14 +0530

    feat: add navbar

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Flex, Text } from "@chakra-ui/react";
 import logo from "../../assets/logo.svg";
 
-export function Navbar() {
+function Navbar() {
   return (
     <Flex align="center" justify="center" wrap="wrap" bg="navBG" px={4} py={1} height={14}>
       <img src={logo} alt="logo" height="40px" width="40px" />
@@ -11,3 +11,5 @@ export function Navbar() {
     </Flex>
   );
 }
+
+export default Navbar;

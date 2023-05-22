8e14a90533cf5853b33cec75b89d1f2145375c41
hokageCV <chaitanyavaru@gmail.com>
2023-05-22 12:33:07 +0530

    feat: take input from user

--- client/src/components/layouts/InputTodo.tsx
+++ client/src/components/layouts/InputTodo.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { Box, IconButton, Center, Input } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { useTodoContextProvider } from "../../context/todo.context";
 
 function InputTodo() {
+  const { addTodo } = useTodoContextProvider();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value);
+
+  const handleSubmit = () => {
+    addTodo(inputValue);
+    setInputValue("");
+  };
+
   return (
     <Box mt="10px">
       <Center gap={1}>
-        <Input placeholder="Add todo" color="cardText" />
-        <IconButton aria-label="Add todo" icon={<AddIcon />} />
+        <Input placeholder="Add todo" color="cardText" value={inputValue} onChange={handleChange} />
+        <IconButton aria-label="Add todo" icon={<AddIcon />} onClick={handleSubmit} />
       </Center>
     </Box>
   );

513793aaa3e95b60aecb38b3f7896db3944f5d74
hokageCV <chaitanyavaru@gmail.com>
2023-05-22 15:22:40 +0530

    feat: add delete option

--- client/src/components/layouts/TodoCard.tsx
+++ client/src/components/layouts/TodoCard.tsx
@@ -1,8 +1,15 @@
 import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import type { Todo } from "../../types";
+import { useTodoContextProvider } from "../../context/todo.context";
 
-function TodoCard({ task }: Todo) {
+function TodoCard({ task, _id }: Todo) {
+  const { deleteTodoItem } = useTodoContextProvider();
+
+  const handleDelete = () => {
+    deleteTodoItem(_id);
+  };
+
   return (
     <Box bg="cardBG" borderRadius={10} m={1} px={3} py={1} minW="300px" maxW="100%">
       <Flex justify="space-between" alignItems="center">
@@ -21,6 +28,7 @@ function TodoCard({ task }: Todo) {
             colorScheme="red"
             variant="ghost"
             size="md"
+            onClick={handleDelete}
           />
         </Box>
       </Flex>
